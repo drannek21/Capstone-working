af9e8ff3e75af32c80600b7450f4bcc784a6fa83
Your drannek21 <delimakennard3@gmail.com>
2025-04-08 12:23:12 +0800

    modal

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -13,6 +13,54 @@ const PrivacyModal = ({ onClose, onProceed }) => (
       <div className="modal-body">
         <p>We are dedicated to protecting your privacy and ensuring the security of your personal information. We collect only the data necessary for providing our services and use it exclusively for those purposes.</p>
         <p>Your information will not be shared with third parties without your consent. We have implemented robust security measures to safeguard your data from unauthorized access or misuse.</p>
+        
+        <div className="required-documents">
+        <p className="required-text">Required Documents</p>
+          <div className="document-grid">
+            <div className="document-column">
+              <div className="document-category">
+                <h5>SINGLE</h5>
+                <ul>
+                  <li>PSA Birth certificate</li>
+                  <li>Income Tax Return</li>
+                  <li>Medical Certificate</li>
+                  <li>CENOMAR</li>
+                </ul>
+              </div>
+              <div className="document-category">
+                <h5>MARRIED</h5>
+                <ul>
+                  <li>PSA Birth certificate</li>
+                  <li>Income Tax Return</li>
+                  <li>Medical Certificate</li>
+                  <li>Marriage Certificate</li>
+                </ul>
+              </div>
+            </div>
+            <div className="document-column">
+              <div className="document-category">
+                <h5>DIVORCE</h5>
+                <ul>
+                  <li>PSA Birth certificate</li>
+                  <li>Income Tax Return</li>
+                  <li>Medical Certificate</li>
+                  <li>Marriage Certificate</li>
+                </ul>
+              </div>
+              <div className="document-category">
+                <h5>WIDOWED</h5>
+                <ul>
+                  <li>PSA Birth certificate</li>
+                  <li>Income Tax Return</li>
+                  <li>Medical Certificate</li>
+                  <li>Marriage Certificate</li>
+                  <li>Death Certificate</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        
         <p className="note-text">Note: Valid e-mail is required</p>
       </div>
       <div className="modal-buttons">
